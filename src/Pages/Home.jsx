7b1ecaf541ac590b2be5470293e302e3758838bd
Oltiariq3561 <oltiariq356@gmail.com>
2024-10-22 16:03:33 +0500

    salomat

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import img1 from '../../public/hero2-2271e3ad.webp';
+import img2 from '../../public/hero1-deae5a1f.webp';
+import { http } from '../axios';
+
+function Home() {
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    http.get(`products?featured=true`)
+      .then(data => {
+        if (data.status === 200) {
+          setProducts(data.data.data);
+        }
+      })
+      .catch(err => console.log(err));
+  }, []); 
+
+  const handleRedirect = (id) => {
+    navigate(`/products/${id}`);
+  };
+
+  const handleProducts = (e) => {
+    e.preventDefault();
+    navigate("/products");
+  };
+
+  return (
+    <div>
+      <div className="flex mb-20">
+        <div className="flex-1">
+          <h1 className="text-7xl ml-10 mr-56 mb-4 font-bold">
+            We are changing <br /> the way people <br /> shop
+          </h1>
+          <p className="ml-10 text-2xl">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br />
+            Tempore repellat explicabo enim soluta temporibus asperiores <br />
+            aut obcaecati perferendis porro nobis.
+          </p>
+          <button onClick={handleProducts} className="ml-10 bg-blue-500 p-3 rounded-md text-white mt-4">
+            OUR PRODUCTS
+          </button>
+        </div>
+        <div className="carousel carousel-center bg-neutral rounded-box max-w-md space-x-4 p-4">
+          <div className="carousel-item">
+            <img src={img1} className="rounded-box h-[420px]" alt="Hero 1" />
+          </div>
+          <div className="carousel-item">
+            <img src={img2} className="rounded-box h-[420px]" alt="Hero 2" />
+          </div>
+        </div>
+      </div>
+
+      <h1 className="text-4xl ml-10 mb-10">Featured Products</h1>
+      <div className="wrapper container mx-auto flex flex-wrap gap-3 justify-center">
+        {products.length > 0 && products.map((product) => (
+          <div key={product.id} className="w-1/4 shadow-md rounded-md cursor-pointer" onClick={() => handleRedirect(product.id)}>
+            <img className="h-[300px] w-full object-cover" src={product.attributes.image} alt={product.attributes.title} />
+            <h2 className="text-lg font-semibold mt-2">{product.attributes.title}</h2>
+            <h3 className="text-xl text-gray-600">${product.attributes.price}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaRegSun } from "react-icons/fa6";
+
+
+function MainLayout({ children }) {
+  const navigate = useNavigate()
+  function handleClick(e) {
+    e.preventDefault()
+    navigate('cart')
+  }
+  return (
+    <div className="bg-[#0a192f] p-4 text-white">
+      <header className="flex justify-between items-center mb-10">
+        <div className="flex items-center space-x-4">
+          <div className="bg-blue-500 p-4 px-5 rounded text-white font-bold">C</div>
+        </div> 
+        <nav className="flex space-x-4">
+          <Link to="/" className="text-gray-400 hover:text-blue-500">Home</Link>
+          <Link to="/about" className="text-gray-400 hover:text-blue-500">About</Link>
+          <Link to="/products" className="text-gray-400 hover:text-blue-500">Products</Link>
+          <Link to="/cart" className="text-gray-400 hover:text-blue-500">Cart</Link>
+        </nav>
+        <div className='wrap flex gap-7'>
+        <FaRegSun className='w-[30px] h-[30px]'/>
+        <div onClick={handleClick} className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-7 w-7"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+
+              <span className="badge badge-sm indicator-item">0</span>
+            </div>
+
+        </div>
+      </header>
+
+      <main>{children}</main>
+    </div>
+  );
+}
+
+export default MainLayout;
